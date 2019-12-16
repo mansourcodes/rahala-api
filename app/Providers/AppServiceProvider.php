04498d2709a590e3236bd58dac3bc1c2b379fa31
Mansour Alnasser <alnassre.it@gmail.com>
2019-12-16 15:04:09 +0300

    passport added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use App\Http\Resources\TripResource;
+use Laravel\Passport\Http\Controllers\AccessTokenController;
 
 
 class AppServiceProvider extends ServiceProvider
